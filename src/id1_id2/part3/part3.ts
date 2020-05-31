b484be84b96aebf0defdee7a126d0504e4219441
speednir123 <50735091+speednir123@users.noreply.github.com>
2020-05-31 10:57:52 +0300

    Biased

--- src/id1_id2/part3/part3.ts
+++ src/id1_id2/part3/part3.ts
@@ -26,6 +26,27 @@ function* braid<T1,T2>(generator1: ()=>Generator<T1>, generator2:()=>Generator<T
     }
 }
 
+function* biased<T1,T2>(generator1: ()=>Generator<T1>, generator2:()=>Generator<T2>){
+    const gen1:Generator<T1> = generator1()
+    const gen2:Generator<T2> = generator2()
+    let curr1 = gen1.next()
+    let curr2 = gen2.next()
+    while(!curr1.done || !curr2.done) {
+        if (!curr1.done) {
+            yield curr1.value
+            curr1 = gen1.next()
+            if(!curr1.done)
+            {
+                yield curr1.value
+                curr1 = gen1.next()
+            }
+        } if(!curr2.done){
+            yield curr2.value
+            curr2 = gen2.next()
+        }
+    }
+}
+
 function* take(n: number, generator:Generator) {
 for (let v of generator){
     if (n <= 0 ) return;
@@ -34,6 +55,6 @@ for (let v of generator){
 }
 }
 
-for (let n of take(4,braid(gen1,gen2))){
+for (let n of take(6,biased(gen1,gen2))){
     console.log(n);
 }
