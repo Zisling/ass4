edee92803cbfd3201804f68bbfc78210a0ecf8ea
speednir123 <50735091+speednir123@users.noreply.github.com>
2020-05-31 12:43:01 +0300

    Biased

--- src/id1_id2/part4/part4.ts
+++ src/id1_id2/part4/part4.ts
@@ -1,17 +1,30 @@
 
-
-const g = (x:number):number=> x*x;
-const f = (x:number):number=> 1/x;
+const g = (x:number):Promise<number>=> {
+    return new Promise<number>((resolve, reject)=>
+    {
+            resolve(x*x)
+    })
+};
+const f = (x:number):Promise<number>=> {
+    return new Promise<number>((resolve, reject)=>{
+        if(x === 0){
+            reject("Error: Division by zero")
+        }
+        else
+            resolve(1/x)
+    })
+}
 
 const h =  (x:number):Promise<number>=> {
     return new Promise<number>((resolve, reject)=>{
-        return f(g(x))
+        g(x).then((num:number)=>f(num)).then((num:number)=>
+        {
+            resolve(num)
+        }).catch((err)=>reject(err))
     })
 }
-const prom = h(6)
-const k = Promise.resolve(prom.then((num:number)=>console.log("Success: "+num)))
-prom.catch((err)=>console.error(err))
-setTimeout(()=>"9",5000);
+const prom = h(0)
+prom.then((num:number)=>console.log("Success: "+num)).catch((err)=>console.log(err))
 
 
 
